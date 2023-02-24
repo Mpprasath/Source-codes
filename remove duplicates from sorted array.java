5cc172415dfea5b763f52db59b32aabba863ec1a
MOHANAPRASATH M <114336287+Mpprasath@users.noreply.github.com>
2023-02-24 20:58:39 +0530

    Create remove duplicates from sorted array.java

---
+++ remove duplicates from sorted array.java
@@ -0,0 +1,21 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+*/
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int t=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[t]){
+                nums[t+1]=nums[i];
+                t++;
+            }
+        }
+        return t+1;
+    }
+}
