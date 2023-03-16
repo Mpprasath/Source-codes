ff9417752f99239340226558c2e5bf465de3dee4
MOHANAPRASATH M <114336287+Mpprasath@users.noreply.github.com>
2023-03-16 14:54:52 +0530

    Create Permutation.cpp

---
+++ Permutation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+            vector<vector<int>> ans;
+        solve(nums,ans,0);
+        return ans;
+    }
+    void solve(vector<int>& nums,vector<vector<int>>&ans,int ind){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            solve(nums,ans,ind+1);
+            swap(nums[i],nums[ind]);
+        }
+    }
+};
